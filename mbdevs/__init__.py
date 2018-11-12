5ab24a8b5973b10df31ae4dbeb29bf6156b8cdfd
Lisin D.A <mrlisdim@gmail.com>
2018-11-12 14:22:42 +0300

    Add CLOSE_PORT_AFTER_EACH_CALL constant to an mb initialization

--- mbdevs/__init__.py
+++ mbdevs/__init__.py
@@ -3,21 +3,25 @@ import minimalmodbus
 
 ModBusDefaults = namedtuple(
     "BotDefaults",
-    ["MB_BAUDRATE", "MB_PARITY", "MB_TIMEOUT"])
+    ["MB_BAUDRATE", "MB_PARITY", "MB_TIMEOUT", "CLOSE_PORT_AFTER_EACH_CALL"])
 
 MB_DEFAULTS = ModBusDefaults(
     MB_BAUDRATE=115200,
     MB_PARITY='N',
-    MB_TIMEOUT=3
-)
+    MB_TIMEOUT=3,
+    CLOSE_PORT_AFTER_EACH_CALL=True)
 
 minimalmodbus.BAUDRATE = MB_DEFAULTS.MB_BAUDRATE
 minimalmodbus.TIMEOUT = MB_DEFAULTS.MB_TIMEOUT
 minimalmodbus.PARITY = MB_DEFAULTS.MB_PARITY
+minimalmodbus.CLOSE_PORT_AFTER_EACH_CALL = MB_DEFAULTS.CLOSE_PORT_AFTER_EACH_CALL
 
-def mb_init(mb_baudrate=MB_DEFAULTS.MB_BAUDRATE,
-            mb_parity=MB_DEFAULTS.MB_PARITY,
-            mb_timeout=MB_DEFAULTS.MB_TIMEOUT):
-    minimalmodbus.BAUDRATE = mb_baudrate
-    minimalmodbus.PARITY = mb_parity
-    minimalmodbus.TIMEOUT = mb_timeout
+
+def mb_init(baudrate=MB_DEFAULTS.MB_BAUDRATE,
+            parity=MB_DEFAULTS.MB_PARITY,
+            timeout=MB_DEFAULTS.MB_TIMEOUT,
+            close_port_after_each_call=MB_DEFAULTS.CLOSE_PORT_AFTER_EACH_CALL):
+    minimalmodbus.BAUDRATE = baudrate
+    minimalmodbus.PARITY = parity
+    minimalmodbus.TIMEOUT = timeout
+    minimalmodbus.CLOSE_PORT_AFTER_EACH_CALL = close_port_after_each_call
