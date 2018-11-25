a277251a9e8ff5dd8885b5f9db09c4e6f27cda19
Dmitriy Lisin <mrlisdim@gmail.com>
2018-11-25 18:55:16 +0000

    Add exception for a fain mb read

--- mbdevs/exceptions.py
+++ mbdevs/exceptions.py
@@ -1,2 +1,7 @@
 class ComDeviceNotFound(IOError):
     pass
+
+class CannotReadARegisterValue(Exception):
+    def __init__(self, reg):
+        msg = "Cannot a value read {} register".format(reg.name)
+        super().__init__(msg)
